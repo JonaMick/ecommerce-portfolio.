f47cbb2bd1d43c10bdcccf43f614d6afcf784d7d
JonaMick <j0n27h2n@gmail.com>
2023-03-18 23:19:07 -0500

    Making responsive desing for home hero

--- ecommerce-portfolio/src/components/AdvantageItem.tsx
+++ ecommerce-portfolio/src/components/AdvantageItem.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 import Image from 'next/image';
 
 type Props = {
@@ -7,13 +7,21 @@ type Props = {
   icon: string;
 }
 export function AdvantageItem({title, content, icon}: Props) {
-    return (<Grid gridTemplateColumns="40px 1fr" alignItems="center" gap="1rem">
+    return (<Grid gridTemplateColumns={{
+      base: '1fr',
+      sm: '40px 1fr'
+    }} alignItems="center" justifyItems='center' gap={{
+      base: '1rem',
+      sm: '0.5rem'
+    }}>
       <GridItem>
         <Image src={icon} width={40} height={40} alt=""></Image>
       </GridItem>
       <GridItem>
         <Text textTransform="uppercase" fontSize="xs" fontWeight="bold">{title}</Text>
-        <Text fontSize="xs">{content}</Text>
+        <Show above='sm'>
+          <Text fontSize="xs">{content}</Text>
+        </Show>
       </GridItem>
     </Grid>);
   }
