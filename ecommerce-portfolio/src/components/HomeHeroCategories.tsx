bdd93a1771d5e6239f05bda4888dcf45d95c9f4c
JonaMick <j0n27h2n@gmail.com>
2023-03-18 22:58:54 -0500

    adding styles to home hero

--- ecommerce-portfolio/src/components/HomeHeroCategories.tsx
+++ ecommerce-portfolio/src/components/HomeHeroCategories.tsx
@@ -12,26 +12,43 @@ type Props = {
 }
 
 export function HomeHeroCategories({ categories }: Props) {
-    return (<Grid templateColumns='540px 255px 255px' gap="30px" templateRows='200px 260px'>
-      {categories.map((cat, key) => {
+    return (<Grid templateColumns={{
+      base: '1fr 1fr',
+      sm: '540px 255px 255px' 
+    }} templateRows={{
+        base: '130px 154px 130px',
+        sm: '200px 260px'
+    }} gap={{
+        base: '0.5rem',
+        sm: '30px'
+    }} templateAreas={{
+      base:  `
+        'cat1 cat1'
+        'cat2 cat3'
+        'cat4 cat4'
+      `,
+      sm: `
+      'cat1 cat2 cat3'
+      'cat1 cat4 cat4'
+      `
+    }}>
+      {categories.map((cat, index) => {
         const slug = slugify(cat);
         const imageUrl = `/pic-categories-${slug}.jpg`;
         
         const gridItemProps: GridItemProps = {
           position: 'relative',
           w: '100%',
-          h: '100%'
+          h: '100%',
+          gridArea: `cat${index + 1}`
         };
   
-        if (key === 0) {
-            gridItemProps.rowSpan = 2;
-        }
-  
-        if (key === categories.length - 1) {
-            gridItemProps.colSpan = 2;
-        }
-  
-        return <GridItem {...gridItemProps} key={key}><Image src={imageUrl} alt={cat} fill={true} /><CenteredLabel>{cat}</CenteredLabel></GridItem>;
+        return <GridItem fontSize={{
+          base: '0.85rem',
+          sm: '1rem'
+        }} position='relative' w='100%' height='100%' gridArea={`cat${index + 1}`} key={index}><Image src={imageUrl} style={{objectFit: 'cover'}} alt={cat} fill={true} />
+        <CenteredLabel>{cat}</CenteredLabel>
+        </GridItem>;
       })}
     </Grid>);
   }
